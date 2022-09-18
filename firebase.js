cb1c03633ed5dd4a37a433204524b7c2e65fa880
rayhan18 <rabu4960@gmail.com>
2022-09-18 17:21:13 +0300

    install firebase and authentication setup

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp ,getApp,getApps} from "firebase/app";
+
+import {getFirestore} from 'firebase/firestore'
+import {getStorege} from 'firebase/storege'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: prosess.env.FIREBASE_API_KEY. ,
+  authDomain: "twitted-clone.firebaseapp.com",
+  projectId: "twitted-clone",
+  storageBucket: "twitted-clone.appspot.com",
+  messagingSenderId: "50797219415",
+  appId: "1:50797219415:web:0e68bfd9dd91312d039d12"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db= getFirestore();
+const  storege =getStorege();
+export {app , db ,storege}
