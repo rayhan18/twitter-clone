ee176a50258b79167b12c311542b9b5e479087d6
rayhan18 <rabu4960@gmail.com>
2022-09-21 14:40:25 +0300

    add like functinality

--- components/Post.js
+++ components/Post.js
@@ -1,14 +1,52 @@
 
+import { async } from "@firebase/util";
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { BsChat, BsHeart, BsShare, BsTrash } from "react-icons/bs";
 import { CgChart } from "react-icons/cg";
 import { HiDotsHorizontal } from "react-icons/hi";
 import Moment from "react-moment";
-
-
+import { db } from "../firebase";
+import { AiFillHeart ,AiOutlineHeart} from "react-icons/ai";
 
 
 export default function Post({allpostdata}) {
-  //console.log(props.allpostdata)
+ const {data:session} = useSession()
+ const [likes ,setLikes] = useState([])
+ const [hasLikes ,setHasLikes] = useState(false)
+
+ useEffect(() => {
+  const unsubscribe = onSnapshot(
+    collection(db, "posts", allpostdata.id, "likes"),
+    (snapshot) => setLikes(snapshot.docs)
+  );
+}, [db]);
+
+
+useEffect(() => {
+  setHasLikes(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+}, [likes]);
+
+
+
+
+
+  async function likePost(){
+    if(session){
+      if(hasLikes){
+        await deleteDoc(doc(db, "posts",allpostdata.id ,'likes',session?.user.uid))
+      }else{
+        await setDoc(doc(db, "posts",allpostdata.id ,'likes' ,session?.user.uid),{
+          username: session.user.username
+        })
+      }
+    }else{
+      signIn()
+    }
+    
+    
+  }
   return (
  <div className="flex p-3 cursor-pointer border-b border-gray-200">
  
@@ -28,7 +66,7 @@ export default function Post({allpostdata}) {
              <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{allpostdata.data().name}</h4>
              <span className="text-sm sm:text-[12px]">@{allpostdata.data().username}</span>
              <span className="text-sm sm:text-[12px] hover:underline"> 
-             <Moment fromNow>{allpostdata?.timestamp?.toDate()}</Moment>
+             <Moment fromNow>{allpostdata?.data().timestamp?.toDate()}</Moment>
              </span>
            </div>
            {/* dot icon */}
@@ -45,7 +83,17 @@ export default function Post({allpostdata}) {
           {/* icons */}
           <div className="flex justify-between text-gray-500 p-2">
             <BsChat className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
-            <BsHeart className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
+            <div className="flex items-center">
+            {hasLikes ?(
+              <AiFillHeart onClick={likePost} className="h-9 w-9 hoverEffect p-2 text-red-500 hover:bg-sky-100"/>
+            ):(
+              <AiOutlineHeart onClick={likePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-sky-100"/>
+            )}
+            {likes?.length > 0 && <span className={`${hasLikes && 'text-red-600 text-sm '}select-none`} >{likes.length}</span>}
+            </div>
+            
+           
+           {/* <AiOutlineHeart onClick={likePost} className="h-9 w-9 hoverEffect p-2  hover:bg-sky-100"/> */}
             <BsTrash className="h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100"/>
             <BsShare className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
             <CgChart className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
